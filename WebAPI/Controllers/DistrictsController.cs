d0e65431e6c34b7076766d17c136ee06fb8ad0c8
yzcmustafa <mustafayazici@ogr.bandirma.edu.tr>
2022-01-04 12:33:14 +0300

    Final WebAPI
    Final WebAPI

---
+++ WebAPI/Controllers/DistrictsController.cs
@@ -0,0 +1,78 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DistrictsController : ControllerBase
+    {
+        private IDistrictService _districtService;
+
+        public DistrictsController(IDistrictService districtService)
+        {
+            _districtService = districtService;
+        }
+
+        [HttpGet("getall")]
+        public IActionResult GetAll()
+        {
+            var result = _districtService.GetAll();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpGet("get")]
+        public IActionResult Get(int id)
+        {
+            var result = _districtService.Get(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpPost("add")]
+        public IActionResult Add(District district)
+        {
+            var result = _districtService.Add(district);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpPost("update")]
+        public IActionResult Update(District district)
+        {
+            var result = _districtService.Update(district);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpPost("delete")]
+        public IActionResult Delete(District district)
+        {
+            var result = _districtService.Delete(district);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+    }
+}
